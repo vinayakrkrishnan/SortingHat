c10363d8d5ff81fbf5eb32883bf61a4c64010c43
Vinayak <vinayak.rkrishnan@gmail.com>
2021-02-01 11:30:06 +0530

    initial commit

---
+++ src/main/java/com/vedantu/SortingHat/App.java
@@ -0,0 +1,13 @@
+package com.vedantu.SortingHat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
